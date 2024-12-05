cdf8285439084af1eee7d0afbc882594a003c6ef
Pavan.D <dpavan.vkumar@gmail.com>
2024-12-06 02:39:21 +0530

    app changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { CountryProvider } from "./context/CountryContext";
 import Countries from "./Components/CountriesTable";
-import States from "./Components/StatesTable";
+
 
 const App: React.FC = () => {
   return (
     <CountryProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Countries />} />
-          <Route path="/:countryName" element={<States />} />
-        </Routes>
-      </Router>
+<Countries/>
     </CountryProvider>
   );
 };
